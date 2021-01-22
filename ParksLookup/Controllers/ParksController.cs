f6ee555e5a56b7dbbf66bc8d9e5e869583746e6a
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-22 10:09:33 -0800

    Add controller

---
+++ ParksLookup/Controllers/ParksController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksLookup.Models;
+
+namespace ParksLookup.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private ParksLookupContext _db;
+
+    public ParksController(ParksLookupContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/parks
+    [HttpGet]
+    public ActionResult<IEnumerable<Park>> Get()
+    {
+      return _db.Parks.ToList();
+    }
+
+    // POST api/parks
+    [HttpPost]
+    public void Post([FromBody] Park park)
+    {
+      _db.Parks.Add(park);
+      _db.SaveChanges();
+    }
+
+    // GET api/parks/1
+    [HttpGet("{id}")]
+    public ActionResult<Park> Get(int id)
+    {
+      return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    }
+
+    // PUT api/parks/1
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+        animal.ParkId = id;
+        _db.Entry(park).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
+    // DELETE api/parks/1
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
