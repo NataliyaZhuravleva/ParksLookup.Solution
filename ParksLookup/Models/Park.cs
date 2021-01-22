0f2cb94687e6aa7fe15cfbad26925f8a8f06f354
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-22 12:20:33 -0800

    Add validation

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 namespace ParksLookup.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string ParkName { get; set; }
+    [Required]
+    [StringLength(10)]
     public string ParkType { get; set; }
+    [Required]
+    [StringLength(50)]
     public string ParkAddress { get; set; }
+    [Required]
     public bool? ParkPetsAllowed { get; set; }
+    [Required]
     public bool? ParkStore { get; set; }
   }
 }
